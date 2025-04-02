cd7a9f220f329f6ac5be073017a956e0ed3dded1
Ziga <ziga.kljun2@gmail.com>
2025-04-02 21:26:07 +0200

    Add operators and make it functional

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 console.log('hello')
+
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.nums');
+const clear = document.querySelector('.clear');
+const operators = document.querySelectorAll('.operators');
+const equal = document.querySelector('.equals');
+
+const btn = buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    display.textContent += button.textContent;
+  });
+});
+
+const operator = operators.forEach(operator => {
+  operator.addEventListener('click', () => {
+    display.textContent += operator.textContent;
+  });
+});
+
+function clearDisplay() {
+  clear.addEventListener('click', () => {
+    display.textContent = '';
+  });
+}
+
+clearDisplay();
+
+function calculateResult() {
+  return eval(display.textContent);
+}
+
+function result() {
+  equal.addEventListener('click', () => {
+    display.textContent = calculateResult();
+  })
+}
+
+result();
