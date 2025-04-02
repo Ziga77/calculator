b9b978c7ce60f8017d4c8eb7ccb073fd20ff4516
Ziga <ziga.kljun2@gmail.com>
2025-04-03 01:04:08 +0200

    Add basic operator functions

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const clear = document.querySelector('.clear');
 const operators = document.querySelectorAll('.operators');
 const equal = document.querySelector('.equals');
 
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+
 const btn = buttons.forEach(button => {
   button.addEventListener('click', () => {
     display.textContent += button.textContent;
@@ -25,15 +42,3 @@ function clearDisplay() {
 }
 
 clearDisplay();
-
-function calculateResult() {
-  return eval(display.textContent);
-}
-
-function result() {
-  equal.addEventListener('click', () => {
-    display.textContent = calculateResult();
-  })
-}
-
-result();
